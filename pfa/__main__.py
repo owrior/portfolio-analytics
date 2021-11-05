dbd3dacf092564cc267b4a664e4be41c2e811d90
owrior <o.w.riordan@gmail.com>
2021-11-05 14:02:40 +0000

    - Refactor existing tables to be config.

--- pfa/__main__.py
+++ pfa/__main__.py
@@ -1,5 +1,5 @@
 from pfa.db_admin import initialise_database
-from pfa.web_access.yahoo_finance import populate_stock_values
+from pfa.web_access.yahoo_finance import populate_yahoo_stock_values
 
 initialise_database()
-populate_stock_values()
+populate_yahoo_stock_values()
--- pfa/web_access/yahoo_finance.py
+++ pfa/web_access/yahoo_finance.py
@@ -4,25 +4,25 @@ from sqlalchemy.orm import Query
 import yfinance as yf
 from typing import List
 
-from pfa.models.date import Date
+from pfa.models.date_config import DateConfig
 from pfa.models.stock_config import StockConfig
-from pfa.models.metric import Metric
+from pfa.models.metric_config import MetricConfig
 
 from pfa.readwrite import read_sql
 from pfa.readwrite import frame_to_sql
 
 
-def populate_stock_values():
+def populate_yahoo_stock_values():
 
-    stock_config, date, metric = _get_config_tables()
+    stock_config, date_config, metric_config = _get_config_tables()
 
     tickers = stock_config["yahoo_ticker"].to_list()
 
     stock_values = _download_and_shape_stock_values(tickers)
 
     stock_values = (
-        stock_values.merge(date, on="date", how="inner")
-        .merge(metric, on="metric", how="inner")
+        stock_values.merge(date_config, on="date", how="inner")
+        .merge(metric_config, on="metric", how="inner")
         .merge(stock_config, on="yahoo_ticker", how="inner")
         .loc[:, ["stock_id", "date_id", "metric_id", "value"]]
     )
@@ -39,11 +39,11 @@ def _get_config_tables():
         )
     )
 
-    date = read_sql(Query(Date))
+    date_config = read_sql(Query(DateConfig))
 
-    metric = read_sql(Query(Metric))
+    metric_config = read_sql(Query(MetricConfig))
 
-    return stock_config, date, metric
+    return stock_config, date_config, metric_config
 
 
 def _download_and_shape_stock_values(tickers: List[str]):
