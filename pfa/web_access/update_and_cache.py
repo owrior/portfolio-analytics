871b5fd220b01dd23779f8c7b377815099145cd3
Oscar Riordan <o.w.riordan@gmail.com>
2021-12-10 22:11:21 +0000

    Or 15 datacache and update (#16)
    * - create update and cache file.
    
    * - Allow for only required data to be downloaded.

---
+++ pfa/web_access/update_and_cache.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sqlalchemy.sql.sqltypes import Date
+from pfa.readwrite import read_sql
+
+from sqlalchemy.orm import Query
+import sqlalchemy as sqa
+
+from pfa.models.stock_config import StockConfig
+from pfa.models.stock_values import StockValues
+from pfa.models.date_config import DateConfig
+
+
+def get_most_recent_stock_dates():
+    stock_dates = read_sql(
+        Query(StockConfig)
+        .with_entities(
+            StockConfig.stock_id,
+            StockConfig.yahoo_ticker,
+            sqa.func.max(StockValues.date_id).label("date_id"),
+            DateConfig.date,
+        )
+        .join(StockValues, StockConfig.stock_id == StockValues.stock_id, isouter=True)
+        .group_by(StockValues.stock_id)
+        .join(DateConfig, StockValues.date_id == DateConfig.date_id, isouter=True)
+    )
+    return stock_dates.loc[:, ["stock_id", "yahoo_ticker", "date"]]
