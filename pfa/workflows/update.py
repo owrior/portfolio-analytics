84919b00af3a2486c25ba0818b02355725abb18d
owrior <o.w.riordan@gmail.com>
2022-06-25 14:35:39 +0100

    Align update with the new functionality.

--- pfa/workflows/update.py
+++ pfa/workflows/update.py
@@ -2,14 +2,15 @@ from prefect import Flow
 from prefect import task
 
 from pfa.web_access.datahub import populate_datahub_parameter_values
+from pfa.web_access.update import get_most_recent_datahub_parameter_dates
+from pfa.web_access.update import get_most_recent_stock_dates
 from pfa.web_access.yahoo_finance import populate_yahoo_stock_values
 
-
-@task(log_stdout=True)
-def update_database():
-    populate_yahoo_stock_values()
-    populate_datahub_parameter_values()
-
-
 with Flow("Update") as flow:
-    update_database()
+    stock_dates = get_most_recent_stock_dates
+    populated_yahoo_stock_values = populate_yahoo_stock_values.map(stock_dates)
+
+    parameter_dates = get_most_recent_datahub_parameter_dates
+    populated_datahub_parameter_values = populate_datahub_parameter_values.map(
+        parameter_dates
+    )
