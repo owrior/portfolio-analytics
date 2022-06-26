804011f3ebedcbb389e15abb6255fa7439348d9d
owrior <o.w.riordan@gmail.com>
2022-06-26 12:52:36 +0100

    Remove old validation functionality.

--- pfa/workflows/validation.py
+++ pfa/workflows/validation.py
@@ -1,5 +1,4 @@
 from prefect import Flow
-from prefect import task
 from prefect import unmapped
 
 from pfa.analytics.bayesian_ridge import validate_baysian_ridge_performance
@@ -7,14 +6,7 @@ from pfa.analytics.data_manipulation import get_date_config
 from pfa.analytics.data_manipulation import get_stock_data_to_forecast
 from pfa.analytics.data_manipulation import get_stock_ids
 from pfa.analytics.prophet import validate_prophet_performance
-from pfa.analytics.validation import run_model_validation
 from pfa.analytics.xgboost import validate_xgboost_performance
-
-
-@task(log_stdout=True)
-def run_analytics():
-    run_model_validation()
-
 
 with Flow("Validation") as flow:
     date_config = get_date_config()
